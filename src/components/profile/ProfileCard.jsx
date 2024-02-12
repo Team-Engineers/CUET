2f9c53f623f323921fbbe99eb759a0cd67698489
Acousticom <omkarsrinivasa.2000@gmail.com>
2024-02-12 20:16:57 +0530

    ui fix

--- src/components/profile/ProfileCard.jsx
+++ src/components/profile/ProfileCard.jsx
@@ -9,7 +9,7 @@ const ProfileCard = () => {
     firstName: "",
     lastName: "",
     email: "",
-    dob:"",
+    dob: "",
     phoneNumber: "",
     location: "",
   });
@@ -22,21 +22,28 @@ const ProfileCard = () => {
   };
 
   return (
-    <div className="w-96 md:w-full">
-      <div className="flex items-center justify-center leading-none md:w-[18%] border-blueviolet-100 border-b-4">
-        <FiArrowLeft size={25} className="text-blueviolet-100" />
-        <h2 className="text-blueviolet-100">Profile</h2>
-      </div>
-      <div className="md:flex md:justify-around md:items-start">
-        <div className="flex justify-center">
-          <img src={profileImg} alt="" className="w-24 md:w-28 xl:w-36" />
+    <div className="w-[500px] md:w-full">
+      <div className="flex items-center justify-center leading-none md:w-[18%] border-blueviolet-100 border-b-4 mt-5">
+        <FiArrowLeft className="text-blueviolet-100 size-7" />
+        <div>
+          <h2 className="text-blueviolet-100 text-5xl xl:text-10xl leading-none">
+            Profile
+          </h2>
+          <div className="flex justify-start w-full">
+            <hr className="bg-blueviolet-100 h-1 border-none w-[40%] start-0" />
+          </div>
         </div>
-        <form className="md:w-[75%] px-2">
-          <p className="text-blueviolet-100 font-semibold text-lg text-center leading-none md:text-left md:w-[75%] underline-offset-1 ">
+      </div>
+      <form className=" px-4 md:flex md:items-start md:justify-around">
+        <div className="flex justify-center md:mt-5">
+          <img src={profileImg} alt="" className="w-24 md:w-28 xl:w-36 " />
+        </div>
+        <div className="md:w-[75%]">
+          <p className="text-blueviolet-100 font-semibold text-lg ">
             Personal Information
           </p>
-          <div className="md:flex md:flex-wrap md:justify-around lg:justify-between ">
-            <div className="flex flex-col mb-3">
+          <div className="md:flex md:flex-wrap md:justify-between ">
+            <div className="flex flex-col mb-5 xl:mb-6">
               <label
                 htmlFor="firstName"
                 className="text-lg before:content-['*'] before:ml-0.5 before:text-red-500 mb-1 text-blueviolet-100 font-semibold "
@@ -52,10 +59,10 @@ const ProfileCard = () => {
                 autoComplete="true"
                 autoCorrect="true"
                 placeholder="Thomas"
-                className="px-6 py-2 text-lg rounded-full border-none shadow-md shadow-gray-500 md:w-52 lg:w-80 xl:w-96"
+                className="px-6 py-2 text-lg rounded-full border-none shadow-md shadow-gray-500 md:w-52 lg:w-80 xl:w-96 text-[#00000080]"
               />
             </div>
-            <div className="flex flex-col mb-3">
+            <div className="flex flex-col mb-5 xl:mb-6">
               <label
                 htmlFor="lastName"
                 className="text-lg before:content-['*'] before:ml-0.5 before:text-red-500 mb-1 text-blueviolet-100 font-semibold"
@@ -71,10 +78,10 @@ const ProfileCard = () => {
                 autoComplete="true"
                 autoCorrect="true"
                 placeholder="J"
-                className="px-6 py-2 text-lg rounded-full border-none shadow-md shadow-gray-500 md:w-52 lg:w-80 xl:w-96"
+                className="px-6 py-2 text-lg rounded-full border-none shadow-md shadow-gray-500 md:w-52 lg:w-80 text-[#00000080] xl:w-96"
               />
             </div>
-            <div className="flex flex-col mb-3">
+            <div className="flex flex-col mb-5 xl:mb-6">
               <label
                 htmlFor="email"
                 className="text-lg before:content-['*'] before:ml-0.5 before:text-red-500 mb-1 text-blueviolet-100 font-semibold"
@@ -90,10 +97,10 @@ const ProfileCard = () => {
                 autoComplete="true"
                 autoCorrect="true"
                 placeholder="Thomas.j@gmail.com"
-                className="px-6 py-2 text-lg rounded-full border-none shadow-md shadow-gray-500 md:w-52 lg:w-80 xl:w-96"
+                className="px-6 py-2 text-lg rounded-full border-none shadow-md shadow-gray-500 md:w-52 lg:w-80 xl:w-96 text-[#00000080]"
               />
             </div>
-            <div className="flex flex-col mb-3">
+            <div className="flex flex-col mb-5 xl:mb-6">
               <label
                 htmlFor="dob"
                 className="text-lg before:content-['*'] before:ml-0.5 before:text-red-500 mb-1 text-blueviolet-100 font-semibold"
@@ -109,10 +116,10 @@ const ProfileCard = () => {
                 autoComplete="true"
                 autoCorrect="true"
                 placeholder="1998-12-5"
-                className="px-6 py-2 text-lg rounded-full border-none shadow-md shadow-gray-500 md:w-52 lg:w-80 xl:w-96"
+                className="px-6 py-2 text-lg rounded-full border-none shadow-md shadow-gray-500 md:w-52 lg:w-80 xl:w-96 text-[#00000080]"
               />
             </div>
-            <div className="flex flex-col mb-3">
+            <div className="flex flex-col mb-5 xl:mb-6">
               <label
                 htmlFor="phoneNumber"
                 className="text-lg before:content-['*'] before:ml-0.5 before:text-red-500 mb-1 text-blueviolet-100 font-semibold"
@@ -128,10 +135,10 @@ const ProfileCard = () => {
                 autoComplete="true"
                 autoCorrect="true"
                 placeholder="+000-000-0000"
-                className="px-6 py-2 text-lg rounded-full border-none shadow-md shadow-gray-500 md:w-52 lg:w-80 xl:w-96"
+                className="px-6 py-2 text-lg rounded-full border-none shadow-md shadow-gray-500 md:w-52 lg:w-80 xl:w-96 text-[#00000080]"
               />
             </div>
-            <div className="flex flex-col mb-3">
+            <div className="flex flex-col mb-5 xl:mb-6">
               <label
                 htmlFor="location"
                 className="text-lg before:content-['*'] before:ml-0.5 before:text-red-500 mb-1 text-blueviolet-100 font-semibold"
@@ -147,21 +154,21 @@ const ProfileCard = () => {
                 autoComplete="true"
                 autoCorrect="true"
                 placeholder="Thomas"
-                className="px-6 py-2 text-lg rounded-full border-none shadow-md shadow-gray-500 md:w-52 lg:w-80 xl:w-96"
+                className="px-6 py-2 text-lg rounded-full border-none shadow-md shadow-gray-500 md:w-52 lg:w-80 xl:w-96 text-[#00000080]"
               />
             </div>
           </div>
-        </form>
-      </div>
+        </div>
+      </form>
       <div className="flex justify-end pb-10">
-        <div className="w-full md:w-[75%] lg:w-[79%]">
+        <div className="w-full md:w-[76%] lg:w-[78%]">
           <div>
             <p className="text-blueviolet-100 font-semibold text-lg">
               Membership
             </p>
             <div className="flex items-center justify-between md:justify-start md:gap-8">
-              <button className="px-6 py-2 text-lg rounded-full border-none bg-white shadow-md shadow-gray-500">
-              <BsFillPersonLinesFill /> CUET GENERAL PACK
+              <button className="px-6 py-2 text-lg rounded-full border-none bg-white shadow-md shadow-gray-500 text-[#00000080]">
+                <BsFillPersonLinesFill /> CUET GENERAL PACK
               </button>
               <button className="px-6 py-2 text-md rounded-full border-none bg-tomato-100 shadow-md shadow-gray-500 text-white text-lg">
                 Upgrade
