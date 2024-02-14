dc6d18ea1812fb14076aa6a62476860021efb585
Acousticom <omkarsrinivasa.2000@gmail.com>
2024-02-15 03:38:10 +0530

    Ui fix login and signup page

--- src/components/Signup/ResponsiveNav.jsx
+++ src/components/Signup/ResponsiveNav.jsx
@@ -1,43 +1,44 @@
 import React from "react";
 import logo from "../../assets/logo_final.png";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import Lottie from "lottie-react";
 import gif from "../../assets/Animation - 1707321568356.json";
 
 const ResponsiveNav = () => {
+  const navigate=useNavigate()
   return (
     <div
-      className="px-5 h-[50vh] sScreens:px-0 sScreens:py-1 sScreens:z-0 sScreens:h-[40vh] w-screen border-2 bg-bgGradientCustom lScreens:hidden  mobiles:h-[30vh] sMobiles:h-[30vh]"
+      className='bg-bgGradientCustom w-full md:hidden'
       style={{ borderRadius: "10% 10% 45% 50% / 0% 0% 42% 44%   " }}
     >
-      <div className="flex justify-between w-full px-3 py-2">
+      <div className="flex justify-between px-2 pt-2">
         <div className="flex gap-2 items-center">
           <img
-            className="h-[40.5px] w-[30px] sScreens:h-[20px]"
+            className="h-[20.5px] w-[25px] sm:h-[40px]"
             loading="eager"
             alt=""
             src={logo}
           />
-          <b className="text-indigo sScreens:text-xs">CUET-TestKnock</b>
+          <b className="text-indigo text-xs sm:text-sm">CUET-TestKnock</b>
         </div>
-        <div className="flex gap-4 items-center sScreens:gap-1">
-          <p className="font-semibold sScreens:text-xs">
+        <div className="flex  items-center gap-1 sm:gap-3">
+          <p className="font-semibold text-xs sm:text-sm">
             Already have an account?{" "}
           </p>
-          <Link
-            className="border-2 border-indigo px-2 py-1 rounded-full text-indigo sScreens:text-xs sScreens:px-1"
-            to="/login"
+          <button
+            className="border-2 border-indigo px-2 py-1 rounded-full text-indigo text-xs no-underline"
+            onClick={()=>navigate('/login')}
           >
             Log In
-          </Link>
+          </button>
         </div>
       </div>
       <div className="">
         <div className="mx-4">
-          <h3 className="text-2xl text-indigo font-bold">Sign-Up</h3>
+          <h3 className="text-[27.8px] text-indigo font-bold sm:text-[30px]">Sign Up</h3>
         </div>
-        <div className="flex justify-center">
-          <Lottie animationData={gif} loop={true} style={{ width: "35%" }} />
+        <div className="flex justify-center mt-0">
+          <Lottie animationData={gif} loop={true} style={{ width: "30%" }} />
         </div>
       </div>
     </div>
