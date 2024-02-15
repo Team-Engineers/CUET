ceb14c2684fda312ce9f539298a792a94dcd48d6
dev967 <devparmar967@gmail.com>
2024-02-15 12:11:39 +0530

    resolved build issues

--- src/App.js
+++ src/App.js
@@ -7,37 +7,27 @@ import AboutPage from "./pages/Aboutpage";
 import Coursespage from "./pages/Coursespage";
 import SubtopicPage from "./components/Courses/SubtopicPage";
 import Learningpage from "./pages/Learningpage";
-import Payment from './pages/Payment';
+import Payment from "./pages/Payment";
 
-
-import Profile from './pages/Profile';
+import Profile from "./pages/Profile";
 function App() {
-  const packname ="CUET GENERAL PACK";
-  const packprice =999;
+  const packname = "CUET GENERAL PACK";
+  const packprice = 999;
   return (
-
-    <Router>
-    <Routes>
-    <Route path="/" element={<Homepage />} />
-
-    </Routes>
-    </Router>
-
     <div>
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/" element={<Homepage />} />
         <Route path="/about" element={<AboutPage />} />
-            <Route path="/profile" element={<Profile />} />
+        <Route path="/profile" element={<Profile />} />
 
         <Route path="/courses" element={<Coursespage />} />
         <Route path="/courses/:subtopic" element={<SubtopicPage />} />
         <Route path="/learning" element={<Learningpage />} />
-<Route path="/payment" element={<Payment packname ={packname} packprice={packprice}/>} />
-          </Routes>
+        <Route path="/payment" element={<Payment packname={packname} packprice={packprice} />} />
+      </Routes>
     </div>
-
   );
 }
 
