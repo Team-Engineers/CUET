75f3f8d440e45891f8e9dee790743a7dc70c2f17
Acousticom <omkarsrinivasa.2000@gmail.com>
2024-02-15 21:30:10 +0530

    syllabus page

--- src/components/Syllabus/SyllabusCard.jsx
+++ src/components/Syllabus/SyllabusCard.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import png from "../../assets/WhatsApp Image 2024-02-15 at 18.41.14.jpeg"
 
 const SyllabusCard = ({subject}) => {
   return (
-    <div className='w-[152.1px] h-[139px] bg-white border-2 m-2 rounded-xl'>
-        <p>{subject}</p>
+    <div className='w-[152.1px] h-[139px] border-2 m-2 rounded-xl shadow-lg shadow-gray-600 bg-white px-2 py-2 flex flex-col items-center justify-center text-center'>
+        <p className='font-bold '>{subject}</p>
+        <img src={png} alt="" />
+        <button className='rounded-full border-none bg-indigo text-white px-2 py-2 font-bold shadow-lg shadow-gray-600'>View/ Download PDF</button>
     </div>
   )
 }
--- src/pages/Syllabus.jsx
+++ src/pages/Syllabus.jsx
@@ -1,15 +1,58 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
-import SyllabusCard from '../components/Syllabus/SyllabusCard'
+import React from "react";
+import Navbar from "../components/Navbar";
+import SyllabusCard from "../components/Syllabus/SyllabusCard";
+import Footer from "../components/Footer";
+import { FiArrowLeft } from "react-icons/fi";
 
 const Syllabus = () => {
-    const subjects=['Accountancy',"Agriculture","Anthropology","Art Education Sculpture","Biology","Business Studies","Chemistry","Computer Science","Econimics/Business Economics","Languages (IA & IB)","Engineering Graphics","Entrepreneurship","Environmental Studies","General Test","Legal Studies","Geography","History","Home Science","Knowledge Tradition -Practices India","Mass Media/ Mass Communication","Mathematics","Performing Arts","Physical Education","Physics","Political Science","Psychology","Sanskrit","Sociology","Teaching Aptitide"]
+  const subjects = [
+    "Accountancy",
+    "Agriculture",
+    "Anthropology",
+    "Art Education Sculpture",
+    "Biology",
+    "Business Studies",
+    "Chemistry",
+    "Computer Science",
+    "Econimics/Business Economics",
+    "Languages (IA & IB)",
+    "Engineering Graphics",
+    "Entrepreneurship",
+    "Environmental Studies",
+    "General Test",
+    "Legal Studies",
+    "Geography",
+    "History",
+    "Home Science",
+    "Knowledge Tradition -Practices India",
+    "Mass Media/ Mass Communication",
+    "Mathematics",
+    "Performing Arts",
+    "Physical Education",
+    "Physics",
+    "Political Science",
+    "Psychology",
+    "Sanskrit",
+    "Sociology",
+    "Teaching Aptitide",
+  ];
   return (
-    <div>
-        <Navbar/>
-        {subjects.map((subject)=><SyllabusCard subject={subject}/>)}
+    <div className="h-full bg-[#c9d4ff]">
+      <Navbar />
+      <div className=" mt-10 mx-24">
+          <h2 className="pl-0 mb-1 text-blueviolet-100 text-5xl xl:text-10xl leading-none">
+            Syllabus
+          </h2>
+          <hr className="bg-blueviolet-100 h-1 border-none w-9 m-0 mb-3" />
+        </div>
+      <div className="flex flex-wrap justify-center mt-10 gap-8 mb-20">
+        {subjects.map((subject) => (
+          <SyllabusCard subject={subject} />
+        ))}
+      </div>
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Syllabus
+export default Syllabus;
