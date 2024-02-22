5e1c19d6c8aa25d455fdd645f65ca41ec7490740
Nitya Dwivedi <96773479+ditya17@users.noreply.github.com>
2024-02-22 17:24:30 +0530

    added links in syllabus page (#34)

--- src/components/Syllabus/SyllabusCard.jsx
+++ src/components/Syllabus/SyllabusCard.jsx
@@ -1,17 +1,22 @@
 import React from "react";
 
 const SyllabusCard = ({ subject }) => {
-  const {subjectName,image}=subject
+  const { subjectName, image, link } = subject;
+
+  const handleButtonClick = () => {
+    window.open(link, "_blank");
+  };
+
   return (
     <div className=" border-2 m-4 rounded-xl shadow-[7px_7px_7px_0px_rgba(0,_0,_0,_0.25)] bg-white ">
       <div className="flex flex-col p-4 items-center justify-evenly text-center">
-      <p className="font-bold max-md:text-[4vw] text-3xl drop-shadow-[7px_7px_7px_rgba(0,_0,_0,_0.25)]">{subjectName}</p>
-      <img src={process.env.PUBLIC_URL +`${image}`} alt="" className="w-32 h-32" />
-      <button className="cursor-pointer rounded-full max-md:text-[1.8vw] text-lg border-none bg-indigo text-white px-5 py-3 font-bold shadow-lg shadow-gray-600">
-        View/ Download PDF
-      </button>
+        <p className="font-bold max-md:text-[4vw] text-3xl drop-shadow-[7px_7px_7px_rgba(0,_0,_0,_0.25)]">{subjectName}</p>
+        <img src={process.env.PUBLIC_URL + `${image}`} alt="" className="w-32 h-32" />
+        <button onClick={handleButtonClick} className="cursor-pointer rounded-full max-md:text-[1.8vw] text-lg border-none bg-indigo text-white px-5 py-3 font-bold shadow-lg shadow-gray-600">
+          View/ Download PDF
+        </button>
       </div>
-      
+
     </div>
   );
 };
--- src/pages/Syllabus.jsx
+++ src/pages/Syllabus.jsx
@@ -1,39 +1,39 @@
 import React from "react";
+import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import SyllabusCard from "../components/Syllabus/SyllabusCard";
-import Footer from "../components/Footer";
 
 const Syllabus = () => {
   const subjects = [
-    {subjectName:"Accounting",image:'/syllabusImages/amico.png'},
-    {subjectName:"Agriculture",image:'/syllabusImages/rice-field/rafiki.png'},
-   { subjectName:"Anthropology",image:'/syllabusImages/Sandy_Edu-04_Single-03 4.png'},
-    {subjectName:"Art Education Sculpture",image:'/syllabusImages/art/cuate.png'},
-    {subjectName:"Biology",image:'/syllabusImages/biologist/rafiki.png'},
-    {subjectName:"Business Studies",image:'/syllabusImages/Business startup statistics and insights.png'},
-    {subjectName:"Chemistry",image:'/syllabusImages/science/pana.png'},
-    {subjectName:"Computer Science",image:'/syllabusImages/researching/amico.png'},
-    {subjectName:"Econimics/Business Economics",image:'/syllabusImages/Financial growth.png'},
-   { subjectName:"Languages (IA & IB)",image:'/syllabusImages/learning-languages/pana.png'},
-    {subjectName:"Engineering Graphics",image:'/syllabusImages/Sandy_Edu-04_Single-03 4.png'},
-    {subjectName:"Entrepreneurship",image:'/syllabusImages/Sandy_Edu-04_Single-03 4.png'},
-    {subjectName:"Environmental Studies",image:'/syllabusImages/nature/cuate.png'},
-    {subjectName:"General Test",image:'/syllabusImages/online-test/rafiki.png'},
-    {subjectName:"Legal Studies",image:'/syllabusImages/law-firm/amico.png'},
-    {subjectName:"Geography",image:'/syllabusImages/Sandy_Edu-04_Single-03 4.png'},
-    {subjectName:"History",image:'/syllabusImages/time-machine/rafiki.png'},
-    {subjectName:"Home Science",image:'/syllabusImages/Sandy_Edu-04_Single-03 4.png'},
-    {subjectName:"Knowledge Tradition -Practices India",image:'/syllabusImages/Sandy_Edu-04_Single-03 4.png'},
-    {subjectName:"Mass Media/ Mass Communication",image:'/syllabusImages/marketing/rafiki.png'},
-    {subjectName:"Mathematics",image:'/syllabusImages/mathematics/amico.png'},
-    {subjectName:"Performing Arts",image:'/syllabusImages/acting/amico.png'},
-    {subjectName:"Physical Education",image:'/syllabusImages/physical-education/pana.png'},
-    {subjectName:"Physics",image:'/syllabusImages/stationery and physics book for education.png'},
-    {subjectName:"Political Science",image:'/syllabusImages/Political debates.png'},
-    {subjectName:"Psychology",image:'/syllabusImages/group-therapy/rafiki.png'},
-    {subjectName:"Sanskrit",image:'/syllabusImages/Sandy_Edu-04_Single-03 4.png'},
-    {subjectName:"Sociology",image:'/syllabusImages/Business statistics.png'},
-    {subjectName:"Teaching Aptitide",image:'/syllabusImages/online-test/rafiki.png'},
+    { subjectName: "Accounting", image: '/syllabusImages/amico.png', link: "https://cdnasb.samarth.ac.in/site-admin23/syllabus/Final+Accountancy+12+March+2023.pdf" },
+    { subjectName: "Agriculture", image: '/syllabusImages/rice-field/rafiki.png', link: "https://cdnasb.samarth.ac.in/site/Syllabus%20of%20CUET%20%28UG%29%20-%202022/302_AGRICULTURE.pdf" },
+    { subjectName: "Anthropology", image: '/syllabusImages/Sandy_Edu-04_Single-03 4.png', link: "https://cdnasb.samarth.ac.in/site/Syllabus%20of%20CUET%20%28UG%29%20-%202022/303_Anthropology.pdf" },
+    { subjectName: "Art Education Sculpture", image: '/syllabusImages/art/cuate.png', link: "https://cdnasb.samarth.ac.in/site/Syllabus%20of%20CUET%20%28UG%29%20-%202022/312_ArtEducation_Painting&Sculpture.pdf" },
+    { subjectName: "Biology", image: '/syllabusImages/biologist/rafiki.png', link: "https://cdnasb.samarth.ac.in/site/Syllabus%20of%20CUET%20%28UG%29%20-%202022/304_BIOLOGY.pdf" },
+    { subjectName: "Business Studies", image: '/syllabusImages/Business startup statistics and insights.png', link: "https://cdnasb.samarth.ac.in/site/Syllabus%20of%20CUET%20%28UG%29%20-%202022/305_BusinessStudeis.pdf" },
+    { subjectName: "Chemistry", image: '/syllabusImages/science/pana.png', link: "https://cdnasb.samarth.ac.in/site/Syllabus%20of%20CUET%20%28UG%29%20-%202022/306_Chemistry.pdf" },
+    { subjectName: "Computer Science", image: '/syllabusImages/researching/amico.png', link: "https://cdnasb.samarth.ac.in/site-admin23/syllabus/Final+Computer+Science+12+March+2023.pdf" },
+    { subjectName: "Economics/Business Economics", image: '/syllabusImages/Financial growth.png', link: "https://cdnasb.samarth.ac.in/site-admin23/syllabus/Final+Economics+12+March+2023.pdf" },
+    { subjectName: "Languages (IA & IB)", image: '/syllabusImages/learning-languages/pana.png', link: "https://cdnasb.samarth.ac.in/site/Syllabus%20of%20CUET%20%28UG%29%20-%202022/LanguageSyllabus(IAandIB).pdf" },
+    { subjectName: "Engineering Graphics", image: '/syllabusImages/Sandy_Edu-04_Single-03 4.png', link: "https://cdnasb.samarth.ac.in/site/Syllabus%20of%20CUET%20%28UG%29%20-%202022/310_Eng.Graphics.pdf" },
+    { subjectName: "Entrepreneurship", image: '/syllabusImages/Sandy_Edu-04_Single-03 4.png', link: "https://cdnasb.samarth.ac.in/site/Syllabus%20of%20CUET%20%28UG%29%20-%202022/311_Entrepreneurship_.pdf" },
+    { subjectName: "Environmental Studies", image: '/syllabusImages/nature/cuate.png', link: "https://cdnasb.samarth.ac.in/site/Syllabus%20of%20CUET%20%28UG%29%20-%202022/307_EnvironmentalStudies.pdf" },
+    { subjectName: "General Test", image: '/syllabusImages/online-test/rafiki.png', link: "https://cdnasb.samarth.ac.in/site-admin23/syllabus/501_General_Test.pdf" },
+    { subjectName: "Legal Studies", image: '/syllabusImages/law-firm/amico.png', link: "https://cdnasb.samarth.ac.in/site/Syllabus%20of%20CUET%20%28UG%29%20-%202022/317_LegalStudies.pdf" },
+    { subjectName: "Geography", image: '/syllabusImages/Sandy_Edu-04_Single-03 4.png', link: "https://cdnasb.samarth.ac.in/site/Syllabus%20of%20CUET%20%28UG%29%20-%202022/313_GEOGRAPHYGEOLOGY.pdf" },
+    { subjectName: "History", image: '/syllabusImages/time-machine/rafiki.png', link: "https://cdnasb.samarth.ac.in/site/Syllabus%20of%20CUET%20%28UG%29%20-%202022/314_History.pdf" },
+    { subjectName: "Home Science", image: '/syllabusImages/Sandy_Edu-04_Single-03 4.png', link: "https://cdnasb.samarth.ac.in/site/Syllabus%20of%20CUET%20%28UG%29%20-%202022/315_HOMESCIENCE.pdf" },
+    { subjectName: "Knowledge Tradition -Practices India", image: '/syllabusImages/Sandy_Edu-04_Single-03 4.png', link: "https://cdnasb.samarth.ac.in/site/Syllabus%20of%20CUET%20%28UG%29%20-%202022/316_KnowledgeTraditions.pdf" },
+    { subjectName: "Mass Media/ Mass Communication", image: '/syllabusImages/marketing/rafiki.png', link: "https://cdnasb.samarth.ac.in/site/Syllabus%20of%20CUET%20%28UG%29%20-%202022/318_MassMedia.pdf" },
+    { subjectName: "Mathematics", image: '/syllabusImages/mathematics/amico.png', link: "https://cdnasb.samarth.ac.in/site-admin23/syllabus/Final_Mathematics+12+March+2023.pdf" },
+    { subjectName: "Performing Arts", image: '/syllabusImages/acting/amico.png', link: "https://cdnasb.samarth.ac.in/site-admin23/syllabus/Final+Performing+Arts+12+March+2023.pdf" },
+    { subjectName: "Physical Education", image: '/syllabusImages/physical-education/pana.png', link: "https://cdnasb.samarth.ac.in/site/Syllabus%20of%20CUET%20%28UG%29%20-%202022/321_PhysicalEducation.pdf" },
+    { subjectName: "Physics", image: '/syllabusImages/stationery and physics book for education.png', link: "https://cdnasb.samarth.ac.in/site-admin23/syllabus/Final+Physics+12+March+2023.pdf" },
+    { subjectName: "Political Science", image: '/syllabusImages/Political debates.png', link: "https://cdnasb.samarth.ac.in/site/Syllabus%20of%20CUET%20%28UG%29%20-%202022/323_politicalscience.pdf" },
+    { subjectName: "Psychology", image: '/syllabusImages/group-therapy/rafiki.png', link: "https://cdnasb.samarth.ac.in/site/Syllabus%20of%20CUET%20%28UG%29%20-%202022/324_Psychology.pdf" },
+    { subjectName: "Sanskrit", image: '/syllabusImages/Sandy_Edu-04_Single-03 4.png', link: "https://cdnasb.samarth.ac.in/site/Syllabus+of+CUET+(UG)+-+2022/325_Sanskrit_Domain.pdf" },
+    { subjectName: "Sociology", image: '/syllabusImages/Business statistics.png', link: "https://cdnasb.samarth.ac.in/site/Syllabus%20of%20CUET%20%28UG%29%20-%202022/326_sociology.pdf" },
+    { subjectName: "Teaching Aptitide", image: '/syllabusImages/online-test/rafiki.png', link: "https://cdnasb.samarth.ac.in/site-admin23/syllabus/327_TeachingAptitude.pdf" },
   ];
   return (
     <div className="w-full  relative bg-[#c9d4ff] overflow-hidden flex flex-col items-center px-0 pb-0 box-border tracking-[normal]">
@@ -55,7 +55,7 @@ const Syllabus = () => {
         </div>
       </div>
       <Footer />
-    </div> 
+    </div>
   );
 };
 
