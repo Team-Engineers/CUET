fa2301fd8fd8fd1dc9c48f7a7480c0d34eb64520
Jimish <jimish957@gmail.com>
2024-02-28 13:08:33 +0530

    completed Price card Design

--- src/components/Packs/PacksPage.jsx
+++ src/components/Packs/PacksPage.jsx
@@ -10,31 +10,38 @@ import { EffectCoverflow, Pagination } from 'swiper/modules';
 import PriceCard from './PriceCard';
 
 export default function PackPage({ isOpen, onClose }) {
+
+  const heightCard = ["450px","470px","500px","470px","450px"];
   return (
     <>
       {isOpen && (
-        <div className="absolute overflow-hidden left-0 w-full flex flex-col justify-center items-center h-screen backdrop-blur-[10px] bg-[#00000045] mx-auto ">
-                      <div className="close-btn cursor-pointer h-[50px] w-[50px] text-black" onClick={onClose}><RxCross1 /></div>
+        <div className="absolute overflow-hidden left-0 w-full flex flex-col justify-center items-center h-max backdrop-blur-[10px] bg-[#00000045] mx-auto my-6">
+          <div className="close-btn cursor-pointer h-[50px] w-[50px] text-black" onClick={onClose}><RxCross1 /></div>
 
-          <div className="">
-            <Swiper
-              effect={'coverflow'}
-              grabCursor={true}
-              centeredSlides={true}
-              slidesPerView={'auto'}
-              pagination={true}
-              modules={[ Pagination]}
-              className="mySwiper h-[500px]"
-            >
-              {[...Array(5)].map((_, index) => (
-                <SwiperSlide key={index}>
-                  <PriceCard />
-                </SwiperSlide>
+            <div className="flex flex-wrap justify-center items-center my-6">
+              {[...Array(5)].map((_, index) => (            
+                <PriceCard height={heightCard[index]} index={index}/>
               ))}
-            </Swiper>
-          </div>
+            </div>
         </div>
       )}
     </>
   );
 }
+
+
+{/* <Swiper
+  effect={'coverflow'}
+  grabCursor={true}
+  centeredSlides={true}
+  slidesPerView={'auto'}
+  pagination={true}
+  modules={[ Pagination]}
+  className="mySwiper h-[500px]"
+>
+  {[...Array(5)].map((_, index) => (
+    <SwiperSlide key={index}>
+      <PriceCard />
+    </SwiperSlide>
+  ))}
+</Swiper> */}
--- src/components/Packs/PriceCard.jsx
+++ src/components/Packs/PriceCard.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
-import './css/PackCard.css'
+// import './css/PackCard.css'
 
-function PriceCard() {
+function PriceCard({height,index}) {
+
   return (
-    <div className=' bg-[#121c4368] backdrop-blur[40px] border-[1px] rounded-2xl border-[#121c43] text-center h-[500px] text-white m-3'>
+    <div 
+      className={` bg-[#121c4368] backdrop-blur[40px] border-[1px] rounded-2xl border-[#121c43] text-center text-white m-1 w-[18rem]`}
+      style=
+      {{
+        height:height,
+        background : (index==2) ? 'linear-gradient(159.85deg, #402E87 9.07%, #402E87 74.25%, #000000 119.2%)' : 'linear-gradient(188.72deg, #5648FC -11.29%, #FFFFFF 111.68%)'
+      }}
+    >
       <div className='p-2 py-4 pt-10'>
-      <h2 className='text-[25px] p-1 w-[80%] mx-auto rounded-3xl text-black bg-[#38d4e2]'>Pack Name</h2>
+      <h2 className='text-[25px] p-1 w-[80%] mx-auto rounded-3xl text-black text-[#5648fc]' 
+        style=
+        {{
+          backgroundColor : (index == 2) ? 'rgba(225, 231, 253, 1)' : 'rgba(217, 217, 217, 1)'
+        }}
+      >Pack Name</h2>
       <h1 className='text-10xl'>Rs 3999</h1>
       <p className='text-[12px]'>
         Supercharge your preparation with
@@ -33,7 +46,15 @@ function PriceCard() {
           </div>
         </div>
       </div>
-      <h4 className='confirmation-btn text-[20px] p-2 mt-14 rounded-3xl mx-auto w-[80%]  bg-[#1754b0]'>Click here to get started</h4>
+      <h4 className='confirmation-btn text-sm p-2 mt-14 rounded-3xl mx-auto w-[80%]  bg-[#1754b0] text-start'
+        style=
+        {{
+          background : (index == 2) ? 'linear-gradient(89.47deg, #562EF7 0.25%, rgba(255, 255, 255, 0.4) 55.02%, #562EF7 109.59%)' : 'rgba(255, 255, 255, 1)',
+          color : (index == 2) ? 'rgba(255, 255, 255, 1)' : 'rgba(86, 72, 252, 1)'
+        }}
+      >
+        Click here to get started
+      </h4>
       </div>
      
     </div>
