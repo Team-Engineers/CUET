9580de11cfaa04072278a045fdbd2d5572b11a28
Ashutosh741 <106524453+Ashutosh741@users.noreply.github.com>
2024-03-05 23:01:37 +0530

    changed font family and api call flexible for both with pagination

--- src/components/prepmodules/PrepModulesQue.jsx
+++ src/components/prepmodules/PrepModulesQue.jsx
@@ -3,72 +3,179 @@ import { MathText } from "../mathJax/MathText";
 
 const PrepModulesQue = ({ data }) => {
   const alphabets = "ABCDEFGHIJKL".split("");
-  const [selectedOptions, setSelectedOptions] = useState(Array(data.length).fill([]));
+  const [selectedOptions, setSelectedOptions] = useState(
+    Array(data.length).fill([])
+  );
   const [currentPage, setCurrentPage] = useState(0);
-  const [explanationsVisible, setExplanationsVisible] = useState(Array(data.length).fill(false));
+  const [explanationsVisible, setExplanationsVisible] = useState(
+    Array(data.length).fill(false)
+  );
 
   const handleOptionClick = (questionIndex, optionIndex) => {
     const updatedSelectedOptions = [...selectedOptions];
-    updatedSelectedOptions[currentPage] = [...updatedSelectedOptions[currentPage]];
+    updatedSelectedOptions[currentPage] = [
+      ...updatedSelectedOptions[currentPage],
+    ];
     updatedSelectedOptions[currentPage][questionIndex] = optionIndex;
 
     setSelectedOptions(updatedSelectedOptions);
   };
 
-
-
   const toggleExplanationVisibility = (questionIndex) => {
     const updatedExplanationsVisible = [...explanationsVisible];
-    updatedExplanationsVisible[questionIndex] = !updatedExplanationsVisible[questionIndex];
+    updatedExplanationsVisible[questionIndex] = !updatedExplanationsVisible[
+      questionIndex
+    ];
     setExplanationsVisible(updatedExplanationsVisible);
   };
 
   const renderQuestion = (question, questionIndex) => {
     const isExplanationVisible = explanationsVisible[questionIndex];
-
+    console.log("question is", question);
     return (
       <div key={questionIndex} className="options-grid">
         <div className="question-box ">
           <div className="flex  flex-col">
-            <div className="flex justify-between">
-              <div className="px-4 py-5">
-                <span className={`question-number id-${question._id}`}>
+            <div className="flex position-absolute top-0 left-0 items-center justify-between">
+              <div className="py-4 px-6">
+                <span className={`question-number id-${question?._id}`}>
                   {`${questionIndex + 1 + currentPage * 5} `}
                 </span>
+              </div>
+            </div>
+            {question?.questionTextAndImages[0]?.text[0] ? (
+              <div className="pt-[2.5rem] pb-2 ">
+                <div className="text-[20px]   pl-8 flex items-center top-[-40px] px-4   ">
+                  <div className="">
+                    {question?.description[0] ? (
+                      <strong>Direction:</strong>
+                    ) : (
+                      ""
+                    )}
+                    {question?.description?.map((desc, descIndex) => (
+                      <MathText
+                        className="question-text mb-2"
+                        key={descIndex}
+                        text={desc}
+                        textTag="h6"
+                      />
+                    ))}
+                  </div>
+                </div>
+                <div className="text-[20px]  pl-8 flex items-center top-[-40px] px-4 ">
+                  {question?.questionTextAndImages?.map(
+                    (textAndImages, textAndImagesIndex) => (
+                      <div className="flex flex-col" key={textAndImagesIndex}>
+                        {textAndImages?.text?.map((text, textIndex) => (
+                          <MathText
+                            className="question-text"
+                            key={textIndex}
+                            text={text}
+                            textTag="h6"
+                          />
+                        ))}
+                        {textAndImages?.image ? (
+                          <img
+                            className="question-image"
+                            key={textAndImagesIndex}
+                            src={textAndImages?.image}
+                            alt={`Img ${textAndImagesIndex + 1}`}
+                          />
+                        ) : (
+                          ""
+                        )}
+                      </div>
+                    )
+                  )}
+                </div>
+              </div>
+            ) : (
+              ""
+            )}
 
-              </div>
-            </div>
-            <div className="text-[20px]  relative pl-8 flex items-center top-[-40px] px-4   ">
-              <div>
-                {question.subQuestions[0].questionTextAndImages.map((textData, textIndex) => (
-                  <MathText
-                    className="question-text mb-2"
-                    key={textIndex}
-                    text={textData.text}
-                    textTag="h6"
-                  />
-                ))}
-              </div>
-            </div>
+            {question?.subQuestions[0]?.questionTextAndImages?.map(
+              (textAndImages, textAndImagesIndex) => (
+                <div
+                  className="flex ps-3 pt-[2.5rem] pb-2 justify-start flex-col"
+                  key={textAndImagesIndex}
+                >
+                  {textAndImages?.text?.map((text, textIndex) => (
+                    <MathText
+                      className="question-text mb-2"
+                      key={textIndex}
+                      text={text}
+                      textTag="h6"
+                    />
+                  ))}
+                  {textAndImages?.image ? (
+                    <img
+                      className="question-image"
+                      key={textAndImagesIndex}
+                      src={textAndImages?.image}
+                      alt={`Img ${textAndImagesIndex + 1}`}
+                    />
+                  ) : (
+                    ""
+                  )}
+                </div>
+              )
+            )}
           </div>
         </div>
-        {question.subQuestions[0].options.map((option, optionIndex) => (
+        {/* <div className=" question-box">
+          <div>
+            <div className="question-text ">
+              {question?.subQuestions[0]?.questionTextAndImages?.map(
+                (textAndImages, textAndImagesIndex) => (
+                  <div className="flex flex-col" key={textAndImagesIndex}>
+                    {textAndImages?.text?.map((text, textIndex) => (
+                      <MathText
+                        className="question-text mb-2"
+                        key={textIndex}
+                        text={text}
+                        textTag="h6"
+                      />
+                    ))}
+                    {textAndImages?.image ? (
+                      <img
+                        className="question-image"
+                        key={textAndImagesIndex}
+                        src={textAndImages?.image}
+                        alt={`Img ${textAndImagesIndex + 1}`}
+                      />
+                    ) : (
+                      ""
+                    )}
+                  </div>
+                )
+              )}
+            </div>
+          </div>
+        </div> */}
+        {question?.subQuestions[0]?.options?.map((option, optionIndex) => (
           <div
             key={optionIndex}
             className={`option-box   
-    ${selectedOptions[currentPage] && selectedOptions[currentPage][questionIndex] === optionIndex ? (question.subQuestions[0].correctOptionIndex === optionIndex ? 'correct' : 'incorrect') : ''}`}
+    ${
+      selectedOptions[currentPage] &&
+      selectedOptions[currentPage][questionIndex] === optionIndex
+        ? question?.subQuestions[0]?.correctOptionIndex === optionIndex
+          ? "correct"
+          : "incorrect"
+        : ""
+    }`}
             onClick={() => handleOptionClick(questionIndex, optionIndex)}
           >
-            <span className="option-alphabet px-2">
+            <span className="option-alphabet text-black px-2">
               {alphabets[optionIndex]}
             </span>
             <div className="flex mx-3 text-[20px]  relative  justify-start gap-3 w-100 items-center ">
-              <MathText text={option.text} textTag="h6" />
+              <MathText text={option?.text} textTag="h6" />
               <div className="single-image-container">
-                {option.image && (
+                {option?.image && (
                   <img
                     className="question-image"
-                    src={option.image}
+                    src={option?.image}
                     alt={`Img ${optionIndex + 1}`}
                   />
                 )}
@@ -78,14 +185,14 @@ const PrepModulesQue = ({ data }) => {
               {selectedOptions[currentPage] &&
                 selectedOptions[currentPage][questionIndex] === optionIndex && (
                   <span>
-                    {question.subQuestions[0].correctOptionIndex === optionIndex ? (
+                    {question?.subQuestions[0]?.correctOptionIndex ===
+                    optionIndex ? (
                       <i className="fa-solid fa-check"></i>
                     ) : (
-                      <i className="fa-solid fa-xmark"></i>
+                      <i className="fa-solid fa-xmark text-black"></i>
                     )}
                   </span>
-                )
-              }
+                )}
             </div>
           </div>
         ))}
@@ -101,34 +208,91 @@ const PrepModulesQue = ({ data }) => {
           <div className="explanation-wrapper ">
             {isExplanationVisible && (
               <div className="explanation">
-                {question.subQuestions[0].explanation.map((explanationData, index) => (
-                  <p className="m-0 pt-3" key={index}>
-                    <h6>
-                      <span dangerouslySetInnerHTML={{ __html: explanationData.text[0] }} />
-                      <div dangerouslySetInnerHTML={{ __html: explanationData.text[1] }} />
-                      <div />
-                      <span dangerouslySetInnerHTML={{ __html: explanationData.text[2] }} />
-                    </h6>
-                    {explanationData.text.slice(3).map((text, index) => (
-                      <MathText key={index} text={text} textTag="h6" />
-                    ))}
-                  </p>
-                ))}
+                {question?.subQuestions[0]?.explanation?.map(
+                  (explanation, explanationIndex) => (
+                    <div
+                      key={explanationIndex}
+                      className="explanation-box"
+                      style={{ margin: "0 20px" }}
+                    >
+                      <div className="flex flex-row gap-2 justify-start items-center">
+                        <h6 className="mb-0 text-primary fw-bold">Answer:</h6>
+                        <h6 className="mb-0  fw-bold text-secondary">
+                          Option{" "}
+                          {question?.subQuestions[0]?.correctOptionIndex !==
+                          undefined
+                            ? alphabets[
+                                data[currentPage]?.subQuestions[0]
+                                  ?.correctOptionIndex
+                              ]
+                            : ""}
+                        </h6>
+                      </div>
+                      <h6 className="text-primary fw-bold">Solution:</h6>
+                      {explanation.text.map((text, textIndex) => (
+                        <MathText
+                          className="explanation-text mb-2"
+                          key={textIndex}
+                          text={text}
+                          textTag="h6"
+                        />
+                      ))}
+                      {explanation?.image ? (
+                        <img
+                          className="question-image"
+                          src={explanation?.image}
+                          alt={`Img ${explanationIndex + 1}`}
+                        />
+                      ) : (
+                        ""
+                      )}
+                    </div>
+                  )
+                )}
               </div>
             )}
           </div>
         </div>
-
       </div>
     );
   };
   const generatePageNumbers = () => {
     const totalPages = Math.ceil(data.length / 5);
+    const maxPagesToShow = 5;
     const pages = [];
-    for (let i = 0; i < totalPages; i++) {
+    let startPage = currentPage - Math.floor(maxPagesToShow / 2);
+    let endPage = currentPage + Math.floor(maxPagesToShow / 2);
+
+    if (startPage < 0) {
+      endPage -= startPage;
+      startPage = 0;
+    }
+
+    if (endPage > totalPages - 1) {
+      startPage -= endPage - totalPages + 1;
+      endPage = totalPages - 1;
+    }
+
+    startPage = Math.max(startPage, 0);
+
+    for (let i = startPage; i <= endPage; i++) {
       pages.push(i);
     }
+
+    while (pages.length > maxPagesToShow) {
+      if (currentPage - startPage <= endPage - currentPage) {
+        pages.pop();
+      } else {
+        pages.shift();
+      }
+    }
+
     return pages;
+  };
+
+  const handlePageChange = (pageIndex) => {
+    setCurrentPage(pageIndex);
+    window.scrollTo(0, 0);
   };
 
   return (
@@ -140,29 +304,64 @@ const PrepModulesQue = ({ data }) => {
             .map((question, questionIndex) =>
               renderQuestion(question, questionIndex)
             )}
-          <div className="pagination max-w-[700px] mx-auto ">
+          {/* <div className="pagination  mx-auto ">
             <button
-              className={`page-button h-[50px] ${currentPage === 0 ? "disabled" : ""}`}
-              onClick={() => setCurrentPage((prev) => prev - 1)}
+              className={`page-button h-[50px] ${
+                currentPage === 0 ? "disabled" : ""
+              }`}
+              onClick={() => handlePageChange(currentPage - 1)}
             >
               Prev
             </button>
-            <div className="max-w-[700px] content-center self-center align-center justify-center  grid grid-cols-8 gap-1 mx-auto">
-              {generatePageNumbers().map((pageIndex, index) => (
+            <div className="content-center self-center align-center justify-center flex flex-wrap gap-1 mx-auto">
+              {generatePageNumbers()?.map((pageIndex, index) => (
                 <button
                   key={pageIndex}
-                  className={`page-button ${currentPage === pageIndex ? "active" : ""}`}
-                  onClick={() => setCurrentPage(pageIndex)}
+                  className={`page-button ${
+                    currentPage === pageIndex ? "active" : ""
+                  }`}
+                  onClick={() => handlePageChange(pageIndex)}
                 >
                   {pageIndex + 1}
                 </button>
               ))}
             </div>
 
-
             <button
-              className={`page-button h-[50px] ${currentPage === Math.ceil(data.length / 5) - 1 ? "disabled" : ""}`}
-              onClick={() => setCurrentPage((prev) => prev + 1)}
+              className={`page-button h-[50px] ${
+                currentPage === Math.ceil(data?.length / 5) - 1
+                  ? "disabled"
+                  : ""
+              }`}
+              onClick={() => handlePageChange(currentPage + 1)}
+            >
+              Next
+            </button>
+          </div> */}
+
+          <div className="pagination">
+            <button
+              className={`page-button ${currentPage === 0 ? "disabled" : ""}`}
+              onClick={() => handlePageChange(currentPage - 1)}
+            >
+              Prev
+            </button>
+            {generatePageNumbers().map((pageIndex) => (
+              <button
+                key={pageIndex}
+                className={`page-button ${
+                  currentPage === pageIndex ? "active" : ""
+                }`}
+                onClick={() => handlePageChange(pageIndex)}
+              >
+                {pageIndex + 1}
+              </button>
+            ))}
+            <button
+              className={`page-button ${
+                currentPage === Math.ceil(data.length / 5) - 1 ? "disabled" : ""
+              }`}
+              onClick={() => handlePageChange(currentPage + 1)}
             >
               Next
             </button>
--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import "./index.css";
 import App from "./App";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { BrowserRouter } from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop/ScrollToTop";
 import { AuthProvider } from "./utils/context";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <GoogleOAuthProvider clientId="162611105850-o634ppn5ner3fi66adr78eg6vmmjqttv.apps.googleusercontent.com">
   <AuthProvider>
     <BrowserRouter>
+    <ScrollToTop/>
       <App />
     </BrowserRouter>
   </AuthProvider>
