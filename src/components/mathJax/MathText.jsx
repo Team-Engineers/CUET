1e34f78e1b5374b412927100580218b27eed70e0
Ashutosh741 <106524453+Ashutosh741@users.noreply.github.com>
2024-03-18 21:20:27 +0530

    removed break space

--- src/components/mathJax/MathText.jsx
+++ src/components/mathJax/MathText.jsx
@@ -7,34 +7,34 @@ export const MathText = ({ text, textTag = "p" }) => {
   if (typeof text !== "string") {
     text = text.toString();
   }
-
   text = text.trim();
 
   const parts = text.split(/\n/);
 
   const jsxElements = parts.flatMap((part, index) => {
     const hasMathExpression = /\$.+?\$/.test(part);
+
     const hasHTMLTags = /<.*?>/.test(part);
 
     if (hasMathExpression) {
       const subparts = part.split(/\$(.*?)\$/);
 
-      return subparts.map((subpart, subIndex) => {
-        if (subIndex % 2 !== 0) {
-          return (
-            <React.Fragment key={`${index}_${subIndex}`}>
+      return subparts
+        .map((subpart, subIndex) => {
+          if (subIndex % 2 !== 0) {
+            return (
               <MathComponent
+                key={`${index}_${subIndex}`}
                 tex={subpart}
                 display={false}
                 className="math-expression"
               />
-              <br className="minimal-space" />
-            </React.Fragment>
-          );
-        } else {
-          return <span key={`${index}_${subIndex}`}>{subpart}</span>;
-        }
-      });
+            );
+          } else {
+            return <span key={`${index}_${subIndex}`}>{subpart}</span>;
+          }
+        })
+        .concat(<br key={`br_${index}`} className="hidden" />);
     } else if (hasHTMLTags) {
       return [
         <TextTag
@@ -42,14 +42,12 @@ export const MathText = ({ text, textTag = "p" }) => {
           className="text-spacing"
           dangerouslySetInnerHTML={{ __html: part }}
         />,
+        <br key={`br_${index}`} className="hidden" />,
       ];
     } else {
-      // Replace line breaks with a space character
-      const contentWithSpaces = part.replace(/\n/g, " ");
-
       return [
-        <span key={index}>{contentWithSpaces}</span>,
-        <br key={`br_${index}`} className="minimal-space" />,
+        <span key={index}>{part}</span>,
+        <br key={`br_${index}`} className="hidden" />,
       ];
     }
   });
